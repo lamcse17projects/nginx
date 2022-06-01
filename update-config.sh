410a54331ae0d681e8ca7cef7495620f085d53fd
Lam Nguyen <nguyenquanglam3008@gmail.com>
2022-06-01 16:31:27 +0700

    fix missing $ in condition

--- update-config.sh
+++ update-config.sh
@@ -1,7 +1,7 @@
 echo "Checking for grpc config"
 original_grpc=$(md5sum /etc/nginx/sites-enabled/grpc.lamprojects.me | cut -c -32)
 current_grpc=$(md5sum ./grpc.lamprojects.me | cut -c -32)
-if [original_grpc == current_grpc]; then
+if [$original_grpc == $current_grpc]; then
     echo "No changes to grpc config"
 else
     sudo rm -rf /etc/nginx/sites-enabled/grpc.lamprojects.me
@@ -11,7 +11,7 @@ fi
 echo "Checking for jenkins config"
 original_jenkins=$(md5sum /etc/nginx/sites-enabled/jenkins.lamprojects.me | cut -c -32)
 current_jenkins=$(md5sum ./jenkins.lamprojects.me | cut -c -32)
-if [original_jenkins == current_jenkins]; then
+if [$original_jenkins == $current_jenkins]; then
     echo "No changes to jenkins config"
 else
     sudo rm -rf /etc/nginx/sites-enabled/jenkins.lamprojects.me
@@ -21,7 +21,7 @@ fi
 echo "Checking for home config"
 original_home=$(md5sum /etc/nginx/sites-enabled/lamprojects.me | cut -c -32)
 current_home=$(md5sum ./lamprojects.me | cut -c -32)
-if [original_home == current_home]; then
+if [$original_home == $current_home]; then
     echo "No changes to home config"
 else
     sudo rm -rf /etc/nginx/sites-enabled/lamprojects.me

b1e9fb28d54af0381760c7ffa232dec42bca0979
tainguyent <tainguyent@kobiton.com>
2022-06-25 11:42:35 +0700

    Adding rancher

--- update-oracle.sh
+++ update-oracle.sh
@@ -11,3 +11,17 @@ if test -f /etc/nginx/sites-enabled/files.lamprojects.me; then
 else
     sudo cp ./files.lamprojects.me /etc/nginx/sites-enabled/files.lamprojects.me
 fi
+
+echo "Checking for rancher config"
+if test -f /etc/nginx/sites-enabled/rancher.lamprojects.me; then
+    original_files=$(md5sum /etc/nginx/sites-enabled/rancher.lamprojects.me | cut -c -32)
+    current_files=$(md5sum ./rancher.lamprojects.me | cut -c -32)
+    if [[ "$original_files" == "$current_files" ]]; then
+        echo "No changes to rancher config"
+    else
+        sudo rm -rf /etc/nginx/sites-enabled/rancher.lamprojects.me
+        sudo cp ./rancher.lamprojects.me /etc/nginx/sites-enabled/rancher.lamprojects.me
+    fi
+else
+    sudo cp ./rancher.lamprojects.me /etc/nginx/sites-enabled/rancher.lamprojects.me
+fi

e82b4aae672c906fd150a861dd0fbd97a95ede2d
Lam Nguyen <nguyenquanglam3008@gmail.com>
2022-07-16 00:34:54 +0700

    move to k8s

--- update-oracle.sh
+++ update-oracle.sh
@@ -12,16 +12,16 @@ else
     sudo cp ./files.lamprojects.me /etc/nginx/sites-enabled/files.lamprojects.me
 fi
 
-echo "Checking for rancher config"
-if test -f /etc/nginx/sites-enabled/rancher.lamprojects.me; then
-    original_files=$(md5sum /etc/nginx/sites-enabled/rancher.lamprojects.me | cut -c -32)
-    current_files=$(md5sum ./rancher.lamprojects.me | cut -c -32)
+echo "Checking for k8s config"
+if test -f /etc/nginx/sites-enabled/k8s.lamprojects.me; then
+    original_files=$(md5sum /etc/nginx/sites-enabled/k8s.lamprojects.me | cut -c -32)
+    current_files=$(md5sum ./k8s.lamprojects.me | cut -c -32)
     if [[ "$original_files" == "$current_files" ]]; then
-        echo "No changes to rancher config"
+        echo "No changes to k8s config"
     else
-        sudo rm -rf /etc/nginx/sites-enabled/rancher.lamprojects.me
-        sudo cp ./rancher.lamprojects.me /etc/nginx/sites-enabled/rancher.lamprojects.me
+        sudo rm -rf /etc/nginx/sites-enabled/k8s.lamprojects.me
+        sudo cp ./k8s.lamprojects.me /etc/nginx/sites-enabled/k8s.lamprojects.me
     fi
 else
-    sudo cp ./rancher.lamprojects.me /etc/nginx/sites-enabled/rancher.lamprojects.me
+    sudo cp ./k8s.lamprojects.me /etc/nginx/sites-enabled/k8s.lamprojects.me
 fi
